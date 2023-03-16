6e29f07e57ada81db259ccddd36f8b4c937050c0
Sabria Islamx <sabria.islam@gmail.com>
2023-03-15 23:17:10 -0400

    adjusted animation.js

--- animation.js
+++ animation.js
@@ -1,18 +1,24 @@
-const delay = 40; // The delay between each character (in milliseconds)
-let i = 0;
+function typeWriter(id) {
+  let element = document.getElementById(id);
+  let text = element.textContent;
+  let length = text.length;
+  let i = 0;
 
-function typeWriter(text) {
-  if (i < text.length) {
-    document.getElementById("intro-text").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(() => typeWriter(text), delay);
-  }else{
-    // Typewriter animation is done
-    setTimeout(() => {
-      document.getElementById("author-text").style.display = "block";
-    }, 100); // Display after a 1-second delay
+  function type() {
+    element.innerText = text.substring(0, i++);
+    if (i <= length) {
+      setTimeout(type, 50);
+    } else {
+      // Typewriter animation is done
+      setTimeout(() => {
+        document.getElementById("author-text").style.display = "block";
+      }, 100); // Display after a 1-second delay
+    }
   }
-}
 
-// Call the function with a new text
-typeWriter("I have this strange feeling that I'm not myself anymore. It's hard to put into words, but I guess it's like I was fast asleep, and someone came, disassembled me, and hurriedly put me back together again. That sort of feeling.");
+  type();
+}
+// Call the function with the intro-text element's ID
+typeWriter("intro-text");
+
+
